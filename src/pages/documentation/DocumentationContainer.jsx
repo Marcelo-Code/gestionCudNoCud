127c964b5e3531da61529815783b9d53d440fe34
Marcelo <feltesmarcelo@gmail.com>
2025-04-17 22:39:54 -0300

    commit

--- src/pages/documentation/DocumentationContainer.jsx
+++ src/pages/documentation/DocumentationContainer.jsx
@@ -21,7 +21,7 @@ import { Documentation } from "./Documentation";
 import {
   patientDocumentData,
   professionalDocumentData,
-} from "../../data/documentsData";
+} from "../../data/documentsData.js";
 
 export const DocumentationContainer = () => {
   //hooks para los datos del formulario
